ddf17a3811b2cd53ad65973410f2cdc6f2564d37
User <mp@smart-bit.ru>
2026-07-03 14:06:24 +0300

    commit start

--- app/src/main/java/com/resume/project/StartActivity.kt
+++ app/src/main/java/com/resume/project/StartActivity.kt
@@ -1,3 +1,15 @@
 package com.resume.project
 
-class StartActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.resume.project.databinding.ActivityStartBinding
+
+class StartActivity: AppCompatActivity() {
+    private  lateinit var binding: ActivityStartBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+    }
+}
