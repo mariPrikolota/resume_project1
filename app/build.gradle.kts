e3f8ae5eb191ff36113b9ff256b8309421ecd475
User <mp@smart-bit.ru>
2026-07-29 15:39:05 +0300

    commit start

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+  //  alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -30,9 +30,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+
 
     buildFeatures{
         viewBinding = true
